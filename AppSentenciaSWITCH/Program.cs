a7065141eebf4fc66f63e32fc1b2e8b791ff062a
remendozag <remendozag@gmail.com>
2020-03-02 08:38:13 -0500

    CORENET
    Sentencia SWITCH

---
+++ AppSentenciaSWITCH/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AppSentenciaSWITCH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+            Console.WriteLine("PROGRAMA SENTENCIA SWITCH");
+            Console.WriteLine("Escriba numero en castellano:");
+            numero = Console.ReadLine();
+            switch (numero.ToUpper()) 
+            {
+                case "UNO":
+                    Console.WriteLine("1");
+                    break;
+                case "DOS":
+                    Console.WriteLine("2");
+                    break;
+                case "TRES":
+                    Console.WriteLine("3");
+                    break;
+                case "CUATRO":
+                    Console.WriteLine("4");
+                    break;
+                case "CINCO":
+                    Console.WriteLine("5");
+                    break;
+                default:
+                    Console.WriteLine("NO SEA BRUTO SOLO NUMEROS!!!");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
